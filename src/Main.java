f98d275a036c6cbbf2a46d23ba634379f19d4ab4
madhurajayashanka <madhuraweerasooriye@gmail.com>
2022-08-12 22:34:02 +0530

    2

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,9 @@ import LinkedList.LinkedList;
 
 public class Main {
     public static void main(String[] args) {
-        LinkedList linkedList = new LinkedList(4);
-        linkedList.getHead();
-        linkedList.getTail();
-        linkedList.getLength();
-        linkedList.append(5);
-        linkedList.append(7);
-        linkedList.append(8);
+        LinkedList linkedList = new LinkedList(1);
         linkedList.printList();
         System.out.println("_________________");
-        System.out.println(linkedList.get(56));
+        linkedList.printList();
     }
 }
