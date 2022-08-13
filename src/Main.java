64d3c693a3d02a61895d78188241e908229e8dc8
madhurajayashanka <madhuraweerasooriye@gmail.com>
2022-08-13 09:59:40 +0530

    2

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,17 @@
-import LinkedList.LinkedList;
+import Doubly_Linked_List.DoublyLinkedList;
 
 public class Main {
     public static void main(String[] args) {
-        LinkedList linkedList = new LinkedList(1);
-        linkedList.printList();
+        DoublyLinkedList DLL = new DoublyLinkedList(3);
+        DLL.getHead();
+        DLL.getTail();
+        DLL.getLength();
+        DLL.append(5);
+        DLL.append(7);
+        DLL.append(23);
+        DLL.printList();
         System.out.println("_________________");
-        linkedList.printList();
+        DLL.remove(1);
+        DLL.printList();
     }
 }
