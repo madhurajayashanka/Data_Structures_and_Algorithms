b2cb825506b901876682f9556ca081150cc91d23
madhurajayashanka <madhuraweerasooriye@gmail.com>
2022-08-14 11:25:32 +0530

    2

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,5 @@
-import BinarySearchTree.BinarySearchTree;
-import Graph.Graph;
-import Hashtable.HashTable;
-import Recursion.Recursion;
-
-import java.util.HashMap;
-
 public class Main {
     public static void main(String[] args) {
-         Recursion recursion =new Recursion();
-
-         System.out.println(recursion.factorial(4));
-         System.out.println("---------------");
 
 
     }
