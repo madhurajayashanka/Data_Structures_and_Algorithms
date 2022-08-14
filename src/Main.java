6ab14baa1973a5f0e6e11ceb98cad1f9d6710f80
madhurajayashanka <madhuraweerasooriye@gmail.com>
2022-08-14 07:47:18 +0530

    2

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,23 @@
 import BinarySearchTree.BinarySearchTree;
+import Graph.Graph;
 import Hashtable.HashTable;
 
 import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
-        HashTable table = new HashTable();
-        table.set("king",2);
-        table.set("donkey",43);
-        table.set("shrek",37);
-        table.set("elza",21);
-        table.printTable();
-        System.out.println(table.keys());
+         Graph graph = new Graph();
+         graph.addVertex("A");
+         graph.addVertex("B");
+         graph.addVertex("C");
+         graph.addVertex("D");
+         graph.addEdge("A","B");
+         graph.addEdge("A","C");
+         graph.addEdge("C","D");
+         graph.printGraph();
          System.out.println("---------------");
+         graph.removeVertex("C");
+         graph.printGraph();
 
 
     }
